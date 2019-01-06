88f552f1ef91ea11d2ca7e91d7faadae8af05dd8
Mandeep3838 <44362185+Mandeep3838@users.noreply.github.com>
2019-01-06 21:50:16 +0530

    Add files via upload

---
+++ img2.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  6 12:08:33 2019
+
+@author: mandeep bawa
+"""
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+import numpy as np
+from glob import glob
+from PIL import ImageFile
+import random
+from tqdm import tqdm
+from keras.preprocessing import image
+from keras.utils import np_utils
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Dropout, Flatten, Dense
+from keras.models import Sequential
+from keras.callbacks import ModelCheckpoint
+
+random.seed(340958234985)
+np.random.seed(2093846)
+
+
+
+image_names = [item.replace('training/', '') for item in sorted(glob("training/*/"))]
+number_of_image_categories = len(image_names)
+print('%d image categories.' % number_of_image_categories)
+# print('Three categories:')
+# print(image_names[:3])
+
+def load_dataset(path):
+    data = load_files(path)
+    image_files = np.array(data['filenames'])
+    image_targets = np_utils.to_categorical(np.array(data['target']), number_of_image_categories)
+    return image_files, image_targets
+
+
+image_files, image_targets = load_dataset('training')
+
+trains_validate_files, test_files, trains_validate_targets, test_targets = \
+    train_test_split(image_files, image_targets, test_size=0.20, random_state=42)
+
+train_files, valid_files, train_targets, valid_targets = \
+    train_test_split(trains_validate_files, trains_validate_targets, test_size=0.02, random_state=42)
+
+image_names = [item[20:-1] for item in sorted(glob("training/*/"))]
+
+print('%s images.\n' % len(np.hstack([train_files, valid_files, test_files])))
+print('%d training images.' % len(train_files))
+print('%d validation images.' % len(valid_files))
+print('%d test images.'% len(test_files))
+
+
+
+
+
+def path_to_tensor(img_path):
+
+    img = image.load_img(img_path, target_size=(256, 256))
+    img_array = image.img_to_array(img)
+
+    return np.expand_dims(img_array, axis=0)
+
+def paths_to_tensor(img_paths):
+    
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+
+
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True                 
+
+train_tensors = paths_to_tensor(train_files).astype('float32')/255
+valid_tensors = paths_to_tensor(valid_files).astype('float32')/255
+test_tensors = paths_to_tensor(test_files).astype('float32')/255
+
+
+
+model = Sequential()
+
+model.add(Conv2D(filters=16, kernel_size=3, padding='same',
+                 activation='relu', input_shape=(256, 256, 3)))
+model.add(MaxPooling2D(pool_size=2))
+
+model.add(Conv2D(filters=24, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.1))
+
+model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(filters=50, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.3))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(256, activation='relu'))
+
+
+model.add(Dense(6, activation='softmax'))
+
+
+model.summary()
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+
+
+
+epochs = 25
+
+checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.image_classifier.hdf5',
+                               verbose=1, save_best_only=True)
+
+model.fit(train_tensors, train_targets, validation_data=(valid_tensors, valid_targets),
+          epochs=epochs, batch_size=64, callbacks=[checkpointer], verbose=1)
+
+
+
+
+
+model.load_weights('saved_models/weights.best.image_classifier.hdf5')
+
+predictions = [np.argmax(model.predict(np.expand_dims(tensor, axis=0))) for tensor in test_tensors]
+
+test_accuracy = 100*np.sum(np.array(predictions)==np.argmax(test_targets, axis=1))/len(predictions)
+print('Test accuracy: %.4f%%' % test_accuracy)
